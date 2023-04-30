700514ebd6c23c51db05b5f7bb9f4fb6530b3082
samarawwleticia <samaradddsantos@gmail.com>
2023-04-30 16:32:54 -0300

    Criação da classe utilidades domesticas

---
+++ src/sistema/UtilidadesDomesticas.java
@@ -0,0 +1,36 @@
+package sistema;
+
+public class UtilidadesDomesticas {
+	
+	private String material;
+	private String marca;
+	private String caracteristicas;
+	
+	
+	public void getmaterial(String material) {
+		this.material = material;
+	}
+	
+	public String setmaterial () {
+		return material;
+	}
+	
+	public void getmarca(String marca) {
+		this.marca = marca;
+	}
+
+	public String setmarca(){
+		return marca;
+	}
+
+	
+	public void getcaracteristicas(String caracteristicas) {
+		this.caracteristicas = caracteristicas;
+	}
+	
+	public String setcaracteristicas() {
+		return caracteristicas;
+	}
+		
+	
+}
