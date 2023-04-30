d9332089947835f2c2ac91304be5e3ec9f37de7a
João Pedro Ferreira Alves <jp1792464@gmail.com>
2023-04-30 15:23:26 -0300

    Metodos Empresas
    Adicionei alguns metodos em Empresa, mas são tds inúteis ainda

--- src/sistema/Empresa.java
+++ src/sistema/Empresa.java
@@ -2,18 +2,38 @@ package sistema;
 
 public class Empresa {
 	
+	// Atributos
 	private String razaoSocial;
 	private String cnpj;
 	
+	// Construtor
 	Empresa(String razaoSocial, String cnpj){
 		this.razaoSocial = razaoSocial;
 		this.cnpj = cnpj;
 	}
 	
+	// Métodos
 	public String obterNome(Empresa empresa) {
 		return this.razaoSocial;
 	}
-	public String cnpj(Empresa empresa) {
+	public String obterCnpj(Empresa empresa) {
 		return this.cnpj;
 	}
-}
+	
+	public void gerarFilial(String nomeCidade) {
+		// adicionar o construtor de uma filial qnd existir filial
+	}
+	
+	public void excluirFilial() {
+		// fazer algo pra destruir filiais qnd possível
+	}
+	
+	public void transferirItens() {
+		// ainda n tenho ideia de como farei isso
+	}
+	
+	public void listarItens() {
+		
+	}
+	
+} // Fim da classe Empresa
