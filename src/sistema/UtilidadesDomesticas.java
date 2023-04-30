b16309672f6717c52809ba56ce78e41301afd9e9
samarawwleticia <samaradddsantos@gmail.com>
2023-04-30 16:16:03 -0300

    Criação da classe UtilidadesDomesticas

---
+++ src/sistema/UtilidadesDomesticas.java
@@ -0,0 +1,24 @@
+package sistema;
+
+public class UtilidadesDomesticas {
+	
+	private int tamanho;
+	private String genero;
+	
+	
+
+	public String getgenero() {
+		return genero;
+	}
+	public void setgenero(String genero) {
+		this.genero = genero;
+	}
+	
+	public int gettamanho() {
+		return tamanho;
+	}
+	
+	public void settamanho(int tamanho) {
+		this.tamanho = tamanho;
+	}
+}
