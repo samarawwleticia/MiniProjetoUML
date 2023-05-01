47e803161e84ed8f3a62b65618d3ecf68ca60950
João Pedro Ferreira Alves <jp1792464@gmail.com>
2023-05-01 17:31:41 -0300

    Mudanças
    ch-ch-ch- changesss

--- src/sistema/Principal.java
+++ src/sistema/Principal.java
@@ -5,24 +5,20 @@ public class Principal {
 	public static void main(String[] args) {
 	
 		//Testes dos produtos
-		Produto p1 = new Alimentacao("Melão", 3.45, 43, true);
-		Alimentacao p2 = new Alimentacao("Costela", 14.23, 21, false);
-		Alimentacao p3 = new Alimentacao("Leite", 5.5, 17, false);
+		Alimentacao p1 = new Alimentacao("Leite", 5.5, 17, "Leitinho de vaca pra sua família!", 1, false);
 		
 		System.out.printf("%s %8s %n", "Nome", "Quantidade" );
 		
 		System.out.println(p1.toString());
-		System.out.print(p2.toString());
-		System.out.print(p3.toString());
 		
-		p3.setEntrada(5);
-		p3.setEntrada(-3);
+		p1.setEntrada(5);
+		p1.setEntrada(-3);
 		
-		System.out.println(p3.getQuantidade());
+		System.out.println(p1.getQuantidade());
 		
-		p3.setSaida(3);
+		p1.setSaida(3);
 		
-		System.out.println(p3.getQuantidade());
+		System.out.println(p1.getQuantidade());
 		//System.out.println(p1.getPeso());
 		
 		//Produto p3 = new Alimentacao("sds", 2.4, 23, "dhfhdfuhd");
