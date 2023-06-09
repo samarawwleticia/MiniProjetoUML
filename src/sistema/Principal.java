cc3d1e8e51d9f662aed5a10a89bd2b13c269c1ed
João Pedro Ferreira Alves <jp1792464@gmail.com>
2023-06-09 13:02:26 -0300

    Revert "volte ao normal github desktop!"
    This reverts commit 25917cddf8f654f23420683d54617ce7f241315d.

--- src/sistema/Principal.java
+++ src/sistema/Principal.java
@@ -1,7 +1,5 @@
 package sistema;
 
-import javax.swing.JOptionPane;
-
 public class Principal {
 
 	public static void main(String[] args) {
@@ -14,6 +12,7 @@ public class Principal {
 		*/
 		
 		// Forma nova:
+		
 		e1.adcFilial(new Filial("Taguatinga", e1));
 		e1.adcFilial(new Filial("Riacho Fundo", e1));
 		e1.adcFilial(new Filial("Gama", e1));
@@ -30,12 +29,7 @@ public class Principal {
 		e1.buscarFilial("Riacho Fundo").adcItem(new Vestuario("calça", 100, 7,"sarja", 42, "unissex" ));
 		e1.buscarFilial("Riacho Fundo").adcItem(new Alimentacao("Leite condensado", 5, 1, "blablabla", 7, false));
 		
-		//e1.buscarFilial("Riacho Fundo").listarItens();
-		//e1.listarItensGeral();
-		
-		
-		
-		
-		
+		e1.buscarFilial("Riacho Fundo").listarItens();
+		e1.listarItensGeral();	
 	}
 }
