5f2479b462dd33e29aa8319f56d7744d628f94c8
João Pedro Ferreira Alves <jp1792464@gmail.com>
2023-06-13 18:35:52 -0300

    Ta ficando legal
    Coloquei o Jtable com algumas infos.

--- src/view/InterfaceProduto.java
+++ src/view/InterfaceProduto.java
@@ -8,27 +8,32 @@ public class InterfaceProduto {
 	
 		public static void main(String[] args) {
 		
-		
-		JLabel g = new JLabel(" FILIAL ");
-		JFrame f = new JFrame();
-		JButton b2 = new JButton(" Cadastrar Filial"); 
 		//tem q usar jtable ou jlist 
+		JLabel label = new JLabel("Produtos");
+		JFrame telaInterfaceProduto = new JFrame();
+		JButton botaoCadastrar = new JButton("Cadastrar Produto");
+		String[] cabecalho = {"Produto", "Quantidade", "Filial"};
+		String[][] listaProdutos = {
+				{"Melancia", "10", "Taguatinga"}, {"Blusa", "15", "Gama"}, {"Colher", "100", "Riacho Fundo"},
+		};
+		JTable tabelaProdutos = new JTable(listaProdutos, cabecalho);
+		JScrollPane painelProdutos = new JScrollPane(tabelaProdutos);
 		
-		b2.setBounds(200,200,200,80);
-		b2.setFont(new Font("Arial", Font.BOLD, 15));
-		g.setBounds(260,100,200,80);
-		g.setFont(new Font("Arial", Font.BOLD, 20));
+		label.setBounds(260,100,200,80);
+		label.setFont(new Font("Arial", Font.BOLD, 20));
+		painelProdutos.setBounds(200, 200, 400, 300);
+		botaoCadastrar.setBounds(200,550,200,80);
+		botaoCadastrar.setFont(new Font("Arial", Font.BOLD, 15));
+		telaInterfaceProduto.setBounds(0, 0,  800, 700); // (0, 0,  1920, 1080) pra ficar quase em tela cheia em um monitor full hd
+		telaInterfaceProduto.setLayout(null);
+		telaInterfaceProduto.setVisible(true);
+		
+		telaInterfaceProduto.add(botaoCadastrar);
+		telaInterfaceProduto.add(label);
+		//telaInterfaceProduto.add(tabelaProdutos);
+		telaInterfaceProduto.add(painelProdutos);
+		
+		
 
-		
-		
-		//f.setSize(500,400);
-		f.setBounds(0, 0,  730, 500); // (0, 0,  1920, 1080) pra ficar quase em tela cheia em um monitor full hd
-		f.setLayout(null);
-		f.setVisible(true);
-		f.add(b2);
-		f.add(g);
-
-		
-
-}
-}
+		}//Fim do método main (Não esquecer de apagar o método)
+}//Fim da classe InterfaceProdut
