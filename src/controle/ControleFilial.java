fe62ab3c60909c06c59e555b0176eedb17a7bc2a
samarawwleticia <129631162+samarawwleticia@users.noreply.github.com>
2023-06-20 18:31:24 -0300

    comecei o controlefilial

--- src/controle/ControleFilial.java
+++ src/controle/ControleFilial.java
@@ -1,5 +1,19 @@
 package controle;
 
+import sistema.*;
+
 public class ControleFilial {
+	
+	private Filial[] f;
+	private int qtdFiliais;
+	
+	public ControleFilial(ControleEmpresa e) {
+		
+		f = e.getFiliais();
+		
+	}
+	
+	
+	
 
 }
