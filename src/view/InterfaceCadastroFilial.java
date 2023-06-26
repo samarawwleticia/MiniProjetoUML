9ebfce17fae41cf49d519c4580cd7e4e2baac36d
samarawwleticia <129631162+samarawwleticia@users.noreply.github.com>
2023-06-26 08:20:27 -0300

    coloquei o get selected index + view cadastro

--- src/view/InterfaceCadastroFilial.java
+++ src/view/InterfaceCadastroFilial.java
@@ -2,100 +2,129 @@ package view;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
+import javax.swing.JButton;
 import javax.swing.*;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+import controle.*;
 
-public class InterfaceCadastroFilial implements ActionListener{
+public class InterfaceCadastroFilial {
 	
-	JButton addProduto = new JButton("Novo Produto");
-	JButton salvar = new JButton("Salvar");
-	JButton deletar = new JButton("Deletar");
+	private JFrame telaCadastroFilial;
+	private JLabel cidade = new JLabel("Filial:");
+	private JLabel titulo = new JLabel("Filial");
+	private JLabel produtosCadastradosFilial = new JLabel("Produtos Disponiveis:");
+	private JButton salvar = new JButton("Salvar");
+	private JButton deletar = new JButton("Deletar");
+	private JTextField textoCidade = new JTextField(200);
+	private JTable tabelaProdutos = new JTable();
+	private JScrollPane painelProdutos = new JScrollPane(tabelaProdutos);
+	private static ControleEmpresa empresa;
+	private int posicao;
+	private int opcao;
+	private String s;
+	
+	//private JButton addProduto = new JButton("Novo Produto");
 	
 	
-	InterfaceCadastroFilial() {
+	public void inserirEditar(int op, ControleEmpresa ce,
+			InterfaceFilial f, int pos) {
 		
-		// criando jframe --------------------------------------------------------
-		JFrame telaCadastroFilial = new JFrame("Cadastro Filial");
-		
-		telaCadastroFilial.setBounds(0, 0, 600, 600);
-		telaCadastroFilial.setLayout(null);
-		telaCadastroFilial.setVisible(true);
-		
-		//titulo da pagina
-		
-		JLabel titulo = new JLabel("Filial");
-		
-		telaCadastroFilial.add(titulo);
-		titulo.setBounds(250,10, 200, 80);
-		titulo.setFont(new Font("Arial", Font.BOLD, 20));
-		
-		// espaço para colocar a cidade que a filial pertence
-	
-		JLabel cidade = new JLabel("Cidade");
-		
-		telaCadastroFilial.add(cidade);
-		cidade.setBounds(150, 130, 100, 20);
-		
-		JTextField textoCidade = new JTextField("");
-		
-		telaCadastroFilial.add(textoCidade);
-		textoCidade.setBounds(250, 130, 120, 20);
-		
-		// espaço para colocar o CNPJ da empresa
-		
-		JLabel CNPJ = new JLabel("CNPJ");
-		
-		telaCadastroFilial.add(CNPJ);
-		CNPJ.setBounds(150, 160, 100, 20);
-		
-		JTextField textoCNPJ = new JTextField("");
-		
-		telaCadastroFilial.add(textoCNPJ);
-		textoCNPJ.setBounds(250, 160, 120, 20);
-		
-		// tabela q mostra os produtos na filial
-		
-		JLabel produtosDisponiveis = new JLabel("Produtos Disponiveis:");
-		
-		telaCadastroFilial.add(produtosDisponiveis);
-		produtosDisponiveis.setBounds(150, 190, 150, 20);
-		
-		String[] titulo2 = {"Nome", "Quantidade", "Tipo","Preço"};
-		String[][] listaProdutos = {
-				{"Blusa", "01", "Vestuario", "R$44,90"},{"Paçoquita", "300", "Alimentacao", "R$02,00"},{"Flanela", "03", "Utilidades", "R$15,00"}
-		};
-		JTable tabelaProdutos = new JTable(listaProdutos, titulo2);
-		JScrollPane painelProdutos = new JScrollPane(tabelaProdutos);
-		
-		telaCadastroFilial.add(painelProdutos);
-		painelProdutos.setBounds(150, 210, 300, 200);
-		
-		// botao para colocar mais um produto
+		opcao = op;
+		posicao = pos;
+		empresa = ce;
 			
-		telaCadastroFilial.add(addProduto);
-		addProduto.setBounds(200, 430, 200, 40);
-		addProduto.addActionListener(this);
 		
-		// botao para salvar um produto
-		
-		telaCadastroFilial.add(salvar);
-		salvar.setBounds(100, 480, 180, 40);
+		if (op == 1) s = "Cadastro de Filial";
+		if (op == 2) s = "Detalhes da Filial";
 		
 		
-		//botao para deletar um produto
+		telaCadastroFilial = new JFrame(s);
 		
-		telaCadastroFilial.add(deletar);
-		deletar.setBounds(300, 480, 180, 40);
-		
-	}
-	
-	public void actionPerformed(ActionEvent evento) {
-		Object elemento = evento.getSource();
-		
-		if (elemento == addProduto) {
-			new InterfaceCadastroProduto();
+		if (op == 2) {
+			
+			//colocar a posicao
+			String[] cabecalho = {
+					"Nome", "Quantidade", "Tipo", "Preço"
+			};
+			String[][] coisas = {
+					{"vou", "mudas", "pros", "produtos"}
+			};
+			tabelaProdutos = new JTable(coisas, cabecalho);
+			painelProdutos = new JScrollPane(tabelaProdutos);
+			textoCidade    = new JTextField(String.valueOf(empresa.getNomesFiliais()[pos].toString()));
+			
+
+			
+		} else { //Não preenche com dados
+			
+			String[] cabecalho = {
+					"Nome", "Quantidade", "Tipo", "Preço"
+			};
+			String[][] coisas = {
+					{"", "", "", ""}
+			};
+			tabelaProdutos = new JTable(coisas, cabecalho);
+			painelProdutos = new JScrollPane(tabelaProdutos);
+			textoCidade = new JTextField(200);
+			salvar.setBounds(100, 480, 180, 40);
+			
 		}
 		
+		if (op == 2) {
+			
+			salvar.setBounds(100, 480, 180, 40);
+			deletar.setBounds(245, 480, 180, 40);
+			this.telaCadastroFilial.add(deletar);
+
+			
+		}
+		
+		
+		this.telaCadastroFilial.setLayout(null);
+		this.telaCadastroFilial.setVisible(true);
+		
+		this.telaCadastroFilial.setBounds(0, 0, 600, 600);
+		
+		titulo.setBounds(250,10, 200, 80);
+		cidade.setBounds(150, 130, 100, 20);
+		textoCidade.setBounds(250, 130, 120, 20);
+		produtosCadastradosFilial.setBounds(150, 190, 150, 20);
+		painelProdutos.setBounds(150, 210, 300, 200);
+		salvar.setBounds(100, 480, 180, 40);
+		deletar.setBounds(300, 480, 180, 40);
+		//addProduto.setBounds(200, 430, 200, 40);
+
+		
+		this.telaCadastroFilial.add(titulo);
+		this.telaCadastroFilial.add(cidade);
+		this.telaCadastroFilial.add(textoCidade);
+		this.telaCadastroFilial.add(produtosCadastradosFilial);
+		this.telaCadastroFilial.add(painelProdutos);
+		this.telaCadastroFilial.add(salvar);
+		this.telaCadastroFilial.add(deletar);
+		
+		titulo.setFont(new Font("Arial", Font.BOLD, 20));
+		
+		
+		// espaço para colocar o CNPJ da empresa - talvez eu tire
+		//JLabel CNPJ = new JLabel("CNPJ");
+		//telaCadastroFilial.add(CNPJ);
+		//CNPJ.setBounds(150, 160, 100, 20);
+		//JTextField textoCNPJ = new JTextField("");
+		//telaCadastroFilial.add(textoCNPJ);
+		//textoCNPJ.setBounds(250, 160, 120, 20);
+	
+		
+		// botao para colocar mais um produto
+		//telaCadastroFilial.add(addProduto);
+		//addProduto.addActionListener(this);
+				
 	}
+		
+	
+	
 
 }
--- src/view/InterfaceFilial.java
+++ src/view/InterfaceFilial.java
@@ -3,21 +3,20 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 import javax.swing.event.*;
-import controle.*;
 import controle.ControleEmpresa;
 
 
-public class InterfaceFilial implements ActionListener{
+public class InterfaceFilial implements ActionListener, ListSelectionListener {
 	private JFrame janela;
 	private JLabel titulo;
 	private JButton cadastroFilial;
-	
+	private JButton refreshFilial;
 	private static ControleEmpresa empresa;
+	
 	private JList<String> listaFiliais;
 	private String[] listaNomes = new String[50];
 	
 	public InterfaceFilial(ControleEmpresa ce ) {
-		
 		empresa = ce;
 		
 		listaNomes = new ControleEmpresa().getNomesFiliais();
@@ -26,25 +25,30 @@ public class InterfaceFilial implements ActionListener{
 		janela = new JFrame("Filiais");
 		titulo = new JLabel("Filiais Cadastradas");
 		cadastroFilial = new JButton("Cadastrar Filial");
+		refreshFilial = new JButton("refresh");
 		
 		titulo.setFont(new Font("Arial", Font.BOLD, 20));
+		
 		titulo.setBounds(90, 10, 250, 30);
+		cadastroFilial.setBounds(70, 177, 100, 30);
+		refreshFilial.setBounds(200, 177, 100, 30);
 		listaFiliais.setBounds(20, 50, 350, 120);
+		
 		listaFiliais.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		listaFiliais.setVisibleRowCount(10);
 		
-		cadastroFilial.setBounds(70, 177, 100, 30);
 		
 		janela.setLayout(null);
-		
 		janela.add(titulo);
 		janela.add(listaFiliais);
 		janela.add(cadastroFilial);
+		janela.add(refreshFilial);
 		
 		janela.setSize(400, 250);
 		janela.setVisible(true);
 		
 		cadastroFilial.addActionListener(this);
+		listaFiliais.addListSelectionListener(this);
 		
 		
 		
@@ -54,8 +58,21 @@ public class InterfaceFilial implements ActionListener{
 	public void actionPerformed(ActionEvent ev) {
 			Object src	= ev.getSource();
 			
-			if(src == cadastroFilial)
-				new InterfaceCadastroFilial();
+			if(src == cadastroFilial) {
+				new InterfaceCadastroFilial().inserirEditar(1, empresa, this, 0);
+			}
+	}
+	
+	public void valueChanged(ListSelectionEvent e) {
+		Object src = e.getSource();
+		
+		if(e.getValueIsAdjusting() && src == listaFiliais) {
+			new InterfaceCadastroFilial().inserirEditar(2, empresa,
+					this, listaFiliais.getSelectedIndex());
+			
+		}
+		
+		//adicionar o refresh;
 	}
 	
 }
